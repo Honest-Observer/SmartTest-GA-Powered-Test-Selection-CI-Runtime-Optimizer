1344bfb2ccd54b7b2ea3eae31b1acb841a92697d
Anirudh Sreekumar Nair <119300949+Honest-Observer@users.noreply.github.com>
2026-07-12 22:12:27 +0530

    Code update 1
    Removed sensitive Firebase configuration details.

--- frontend-dashboard/src/config/firebase.js
+++ frontend-dashboard/src/config/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCvXA1BwRG2zpU1CS5wCLIpz0zA7NRej-k',
-  authDomain: 'tia-optimizer.firebaseapp.com',
-  projectId: 'tia-optimizer',
-  storageBucket: 'tia-optimizer.firebasestorage.app',
-  messagingSenderId: '272066519691',
-  appId: '1:272066519691:web:7d264f64c88d2c01302a8e',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 };
 
 const app = initializeApp(firebaseConfig);
